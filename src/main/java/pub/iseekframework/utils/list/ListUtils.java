66b0c22cd9b1f5ac5cef0e76bc41326b1d705c11
wangsong <825582185@qq.com>
2018-05-21 18:52:12 +0800

    提交List公共类代码

---
+++ src/main/java/pub/iseekframework/utils/list/ListUtils.java
@@ -0,0 +1,10 @@
+package pub.iseekframework.utils.list;
+
+/**
+ * desc：     List公共类 <br>
+ * author：   wangsong <br>
+ * date：     2018/5/21 下午6:38 <br>
+ * version：  v1.0.0 <br>
+ */
+public class ListUtils {
+}
