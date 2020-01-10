f582ef8366f4509f77ce3862a82c37d46e8d94fc
wangsong <wawm913489902@163.com>
2020-01-10 15:35:53 +0800

    feat(iseekframework-utils)：新增了DemoUtils.java文件

---
+++ src/main/java/pub/iseekframework/utils/collection/DemoUtils.java
@@ -0,0 +1,16 @@
+package pub.iseekframework.utils.collection;
+
+/**
+ * Package pub.iseekframework.utils.collection<br>
+ * author wangsong<br>
+ * Description: TODO<br>
+ * date 2020/1/10 3:26 下午<br>
+ * version V1.0
+ * @author gaby
+ */
+public class DemoUtils {
+
+    public String getUser(String name) {
+        return name;
+    }
+}
