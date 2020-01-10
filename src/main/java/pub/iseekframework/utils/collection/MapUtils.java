7f11279e1362d02185d03d479dfd432eaf094da1
wangsong <wawm913489902@163.com>
2020-01-10 14:57:29 +0800

    feat(iseekframework-utils)：增加了map集合的公共类

---
+++ src/main/java/pub/iseekframework/utils/collection/MapUtils.java
@@ -0,0 +1,12 @@
+package pub.iseekframework.utils.collection;
+
+/**
+ * Package pub.iseekframework.utils.collection<br>
+ * author wangsong<br>
+ * Description: map集合相关的公共类<br>
+ * date 2020/1/10 2:54 下午<br>
+ * version V1.0
+ * @author gaby
+ */
+public class MapUtils {
+}
