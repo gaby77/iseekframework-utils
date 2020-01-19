48e077f1061f305105a6a99dc083b9bbd7ff3d5e
wangsong <wawm913489902@163.com>
2020-01-19 19:03:24 +0800

    feat(iseekframework-utils)：修改了HttpConnectionManagerTest测试类

--- src/test/java/pub/iseekframework/utils/http/HttpConnectionManagerTest.java
+++ src/test/java/pub/iseekframework/utils/http/HttpConnectionManagerTest.java
@@ -14,13 +14,28 @@ public class HttpConnectionManagerTest {
 
     public static void main(String[] args) {
 
-        Thread th1 = new Thread(new HttpThread(),"线程1");
-        Thread th2 = new Thread(new HttpThread(),"线程2");
-        Thread th3 = new Thread(new HttpThread(),"线程3");
+        doHttp();
 
-        th1.start();
-        th2.start();
-        th3.start();
+//        Thread th1 = new Thread(new HttpThread(),"线程1");
+//        Thread th2 = new Thread(new HttpThread(),"线程2");
+//        Thread th3 = new Thread(new HttpThread(),"线程3");
+//
+//        th1.start();
+//        th2.start();
+//        th3.start();
+
+
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println("======================begin: " + i + "==============================" + "<br>");
+//            doHttp();
+//            System.out.println("======================end: " + i + "==============================" + "<br>");
+//        }
+
+
+        for (int i = 0; i < 1000; i++) {
+           Thread th = new Thread(new HttpThread(), "线程" + i);
+           th.start();
+        }
 
 
     }
@@ -30,31 +45,35 @@ public class HttpConnectionManagerTest {
 
         @Override
         public void run() {
-            String doGetUrl = "https://localhost:8081/api/v1/test?name=wassss";
-            String doLocalePostUrl = "https://localhost:8081/api/v1/getLocale";
-            String doPostUrl = "https://localhost:8081/api/v1/user/addUserInfoOne";
-            String result;
-
-            Map<String, String> headMap = new HashMap<>();
-            headMap.put("token", "111111");
-            headMap.put("lang", "th");
-
-            String bodyJsonStr = "{ \"email\": \"21:03:37test2\", \"gender\": 0, \"mobile\": \"string\", \"nickName\": \"2020年01月18日21:03:37test2\"}";
-
-
-            result =  HttpConnectionManager.sendGetICClient(doGetUrl);
-            System.out.println("get请求不带header，返回结果：" + result);
-
-
-            result = HttpConnectionManager.sendGetICClient(doGetUrl, headMap);
-            System.out.println("get请求带header，返回结果：" + result);
-
-            result = HttpConnectionManager.sendPostICClient(doPostUrl, bodyJsonStr);
-            System.out.println("post请求不带header，返回结果：" + result);
-
-
-            result = HttpConnectionManager.sendPostICClient(doLocalePostUrl, null, headMap);
-            System.out.println("post请求带header，返回结果：" + result);
+            doHttp();
         }
+    }
+
+    public static void doHttp() {
+        String doGetUrl = "http://localhost:8081/api/v1/test?name=wassss";
+        String doLocalePostUrl = "http://localhost:8081/api/v1/getLocale";
+        String doPostUrl = "http://localhost:8081/api/v1/user/addUserInfoOne";
+        String result;
+
+        Map<String, String> headMap = new HashMap<>();
+        headMap.put("token", "111111");
+        headMap.put("lang", "th");
+
+        String bodyJsonStr = "{ \"email\": \"21:03:37test2\", \"gender\": 0, \"mobile\": \"string\", \"nickName\": \"2020年01月18日21:03:37test2\"}";
+
+
+        result =  HttpConnectionManager.sendGetICClient(doGetUrl);
+        System.out.println(Thread.currentThread() + "get请求不带header，返回结果：" + result);
+
+
+        result = HttpConnectionManager.sendGetICClient(doGetUrl, headMap);
+        System.out.println(Thread.currentThread() + "get请求带header，返回结果：" + result);
+
+        result = HttpConnectionManager.sendPostICClient(doPostUrl, bodyJsonStr);
+        System.out.println(Thread.currentThread() + "post请求不带header，返回结果：" + result);
+
+
+        result = HttpConnectionManager.sendPostICClient(doLocalePostUrl, null, headMap);
+        System.out.println(Thread.currentThread() + "post请求带header，返回结果：" + result);
     }
 }
